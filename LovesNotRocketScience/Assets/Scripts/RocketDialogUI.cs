e66240bd95428e1161fb446e415a7be24707906d
Ondřej Paška <ondrakiller@gmail.com>
2019-11-09 18:28:57 +0100

    dialog changes

--- LovesNotRocketScience/Assets/Scripts/RocketDialogUI.cs
+++ LovesNotRocketScience/Assets/Scripts/RocketDialogUI.cs
@@ -7,13 +7,20 @@ public class RocketDialogUI : DialogueUIBehaviour
 {
     public GameObject BubblePrefab;
     public GameObject OptionsPrefab;
+    public Transform Point1;
+    public Transform Point2;
+    public Transform OptionsPoint;
+    public Camera GameObjectCamera;
     
     public override IEnumerator RunLine(Line line)
     {
         print(line.text);
         var bubble = Instantiate(BubblePrefab);
         bubble.GetComponent<DialogBubble>().Text = line.text;
+        bubble.transform.position = Point1.position;
+        GameObjectCamera.gameObject.SetActive(false);
         yield return new WaitForSeconds(line.text.Length*0.05f);
+        GameObjectCamera.gameObject.SetActive(true);
         Destroy(bubble);
     }
 
@@ -24,12 +31,19 @@ public class RocketDialogUI : DialogueUIBehaviour
         {
             print(a);
         }
-        
+
         var optionsp = Instantiate(OptionsPrefab);
+        optionsp.transform.position = OptionsPoint.position;
         var optionsScript = optionsp.GetComponent<RocketDialogOptions>();
         optionsScript.SetOptions(optionsCollection.options);
         yield return optionsScript.WaitForInput(optionChooser);
         
+        var bubble = Instantiate(BubblePrefab);
+        bubble.GetComponent<DialogBubble>().Text = optionsScript.SelectedText;
+        bubble.transform.position = Point2.position;
+        
+        yield return new WaitForSeconds(optionsScript.SelectedText.Length*0.03f);
+        Destroy(bubble);
         Destroy(optionsp);
     }
 
