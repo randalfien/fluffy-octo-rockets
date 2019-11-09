9924cdd39f57945a18e572cc86069dcd428c7ea1
Ondřej Paška <ondrakiller@gmail.com>
2019-11-09 20:43:29 +0100

    remade rocket prefab

---
+++ LovesNotRocketScience/Assets/RocketRotation.cs
@@ -0,0 +1,13 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using Yarn.Unity;
+
+public class RocketRotation : MonoBehaviour
+{
+    [YarnCommand("rotate")]
+    public void Rotate(string howmuch)
+    {
+        float angle = (float) int.Parse(howmuch);
+        transform.DOLocalRotate(new Vector3(0,0,angle), 6).SetEase(Ease.InOutQuad);
+    }
+}
