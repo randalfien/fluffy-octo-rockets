1e475258130786723a174f570a7354dc3bcb4889
Ondřej Paška <ondrakiller@gmail.com>
2019-11-09 19:57:25 +0100

    nebe scripts

---
+++ LovesNotRocketScience/Assets/RocketSky.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class RocketSky : MonoBehaviour
+{
+    [YarnCommand("move")]
+    public void MoveCharacter(string destinationName) {
+       print(destinationName);
+    }
+}
