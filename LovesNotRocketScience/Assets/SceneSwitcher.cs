71d1293c185c49923bf48b9a11491bfb1fa3595e
Ondřej Trhoň <ondrej@trhon.cz>
2019-11-10 17:45:43 +0100

    fixes

--- LovesNotRocketScience/Assets/SceneSwitcher.cs
+++ LovesNotRocketScience/Assets/SceneSwitcher.cs
@@ -6,29 +6,34 @@ using Yarn.Unity;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    private SpriteRenderer MySpriteRenderer;
+
     [YarnCommand("switchscene")]
+    
     public void SwitchScene(string valueval)
     {
-        
-        StartCoroutine(FadeTo(1.0f, 1f));
+                MySpriteRenderer = GetComponent<SpriteRenderer>();
+                MySpriteRenderer.disabled = !MySpriteRenderer.disabled;
+                  StartCoroutine(FadeTo(1f, 4f));
     
         
         if (valueval == "happy")
         {
-            Invoke("SwitchHappy",3f);
+            Invoke("SwitchHappy",5f);
         }
         else
         {
-            Invoke("SwitchSad",3f); 
+            Invoke("SwitchSad",5f); 
         }
     }
+
 IEnumerator FadeTo(float aValue, float aTime)
      {
-         float alpha = gameObject.GetComponent<SpriteRenderer>().material.color.a;
+         float alpha = gameObject.GetComponent<SpriteRenderer>().color.a;
          for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
          {
              Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
-             gameObject.GetComponent<SpriteRenderer>().material.color = newColor;
+             gameObject.GetComponent<SpriteRenderer>().color = newColor;
              yield return null;
          }
      }
