3ad9210fdc99fb2b02d983bc643f3cf3d45ee570
Ondřej Paška <ondrakiller@gmail.com>
2020-02-02 12:17:20 +0100

    mraky fix

---
+++ LovesNotRocketScience/Assets/CloudAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CloudAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.DOLocalMove(new Vector3(-49, -42, -13), 1.8f).OnComplete(DestroyAfter);
+    }
+
+    // Update is called once per frame
+    void DestroyAfter()
+    {
+        Destroy(gameObject);
+    }
+}
